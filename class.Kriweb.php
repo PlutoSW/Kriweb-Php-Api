70656db4ff12099e570c202f6544c9b5aa7dc2bd
PlutoSW <31663648+PlutoSW@users.noreply.github.com>
2018-01-11 20:15:30 +0300

    First Commit
    Get Token, Register Domain, Transfer Domain, Check Domains Availability

---
+++ class.Kriweb.php
@@ -0,0 +1,81 @@
+<?php
+session_start();
+class Kriweb
+{
+    private $apiUrl = "https://kriweb.com/rest/";
+    public function __construct($username, $password)
+    {
+        $this->curl     = curl_init();
+        $this->username = $username;
+        $this->password = $password;
+        $this->checksum = md5($username . $password);
+        if ($_SESSION["KRW-Checksum"] === $this->checksum) {
+            $this->KRWLastTokenTime = ($_SESSION["KRW-LastTokenTime"]) ? $_SESSION["KRW-LastTokenTime"] : 0;
+        } else {
+            $this->KRWLastTokenTime        = 0;
+            $_SESSION["KRW-Token"]         = 0;
+            $_SESSION["KRW-LastTokenTime"] = false;
+            $_SESSION["KRW-Checksum"]      = md5($username . $password);
+        }
+        $this->token = $this->getToken();
+    }
+    public function getToken()
+    {
+        if ($this->KRWLastTokenTime < time()) {
+            $response                      = $this->run("token", array("username" => $this->username, "password" => $this->password));
+            $_SESSION["KRW-Token"]         = $response["token"];
+            $_SESSION["KRW-LastTokenTime"] = strtotime("+500 seconds");
+            return $_SESSION["KRW-Token"];
+
+        } else {
+            return $_SESSION["KRW-Token"];
+        }
+    }
+
+    private function run($operation, $parameters)
+    {
+        $this->curl = curl_init();
+        curl_setopt_array($this->curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL            => $this->apiUrl . $operation,
+            CURLOPT_POST           => 1,
+            CURLOPT_POSTFIELDS     => $parameters,
+            CURLOPT_HTTPHEADER     => array('Token: ' . $this->token),
+        ));
+        $response = json_decode(curl_exec($this->curl), true);
+        if ($response["result"] === "success") {
+            return $response;
+        } else {
+            exit($response["error"]);
+        }
+    }
+
+    public function isAvailable($domains)
+    {
+        if (is_array($domains)) {
+            $domainList = array();
+            foreach ($domains as $domain) {
+                $domainList[] = array("domain" => $domain);
+            }
+        } else {
+            $domainList[] = array("domain" => $domains);
+        }
+        return $this->run("whois", json_encode($domainList))["whoisresult"];
+    }
+    public function register($domain, $year = 1)
+    {
+        $domainInfo = array("domain" => $domain, "type" => "register", "year" => $year);
+
+        return $this->run("domain", json_encode($domainInfo));
+    }
+    public function transfer($domain, $year = 1, $transfercode)
+    {
+        $domainInfo = array("domain" => $domain, "type" => "register", "year" => $year, "epp" => $transfercode);
+
+        return $this->run("domain", json_encode($domainInfo));
+    }
+    public function __destruct()
+    {
+        curl_close($this->curl);
+    }
+}
