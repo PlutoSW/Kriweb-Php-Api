010c8f41ee7933b5917f78b68edec0610d5cfea4
PlutoSW <31663648+PlutoSW@users.noreply.github.com>
2018-01-12 16:08:25 +0300

    Test Mode Enable
    Added Test Mode

--- class.Kriweb.php
+++ class.Kriweb.php
@@ -3,9 +3,13 @@ session_start();
 class Kriweb
 {
     private $apiUrl = "https://kriweb.com/rest/";
-    public function __construct($username, $password)
+    public function __construct()
     {
-        $this->curl     = curl_init();
+        $this->curl = curl_init();
+    }
+
+    public function user($username, $password)
+    {
         $this->username = $username;
         $this->password = $password;
         $this->checksum = md5($username . $password);
@@ -19,6 +23,12 @@ class Kriweb
         }
         $this->token = $this->getToken();
     }
+
+    public function test($condition = false)
+    {
+        $this->test = $condition;
+    }
+
     public function getToken()
     {
         if ($this->KRWLastTokenTime < time()) {
@@ -40,7 +50,7 @@ class Kriweb
             CURLOPT_URL            => $this->apiUrl . $operation,
             CURLOPT_POST           => 1,
             CURLOPT_POSTFIELDS     => $parameters,
-            CURLOPT_HTTPHEADER     => array('Token: ' . $this->token),
+            CURLOPT_HTTPHEADER     => array('Token: ' . $this->token, ($this->test) ? 'Testmode: yes' : ''),
         ));
         $response = json_decode(curl_exec($this->curl), true);
         if ($response["result"] === "success") {
@@ -66,13 +76,13 @@ class Kriweb
     {
         $domainInfo = array("domain" => $domain, "type" => "register", "year" => $year);
 
-        return $this->run("domain", json_encode($domainInfo));
+        return $this->run("domain", json_encode($domainInfo))["domainresult"];
     }
     public function transfer($domain, $year = 1, $transfercode)
     {
         $domainInfo = array("domain" => $domain, "type" => "register", "year" => $year, "epp" => $transfercode);
 
-        return $this->run("domain", json_encode($domainInfo));
+        return $this->run("domain", json_encode($domainInfo))["domainresult"];
     }
     public function __destruct()
     {
